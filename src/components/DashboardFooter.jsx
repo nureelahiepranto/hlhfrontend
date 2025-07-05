d3b3294dbb7c24b8620696c132a7391e78699c69
pranto0000 <elahiepranto@gmail.com>
2025-07-05 19:41:33 +0600

    update

--- src/components/DashboardFooter.jsx
+++ src/components/DashboardFooter.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const DashboardFooter = () => {
   return (
-    <footer className="bg-gray-900 text-white mt-20">
+    <footer className="bg-gray-900 text-white mt-40">
         {/* Copyright Section */}
         <div className="border-t border-gray-800 mt-8 text-center">
           <p className="text-gray-400">
