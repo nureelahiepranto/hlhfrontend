11168e75b4725c60294db61c2605b02f120c1da7
pranto0000 <elahiepranto@gmail.com>
2025-07-27 01:39:05 +0600

    update

--- src/Dashboard/Admin/StudentIDCard.jsx
+++ src/Dashboard/Admin/StudentIDCard.jsx
@@ -69,7 +69,7 @@ const StudentIDCard = () => {
 
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
-      <h1 className="text-3xl font-bold mb-6 text-gray-800">Employee ID Cards</h1>
+      <h1 className="text-3xl font-bold mb-6 text-green-500">Employee ID Cards</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {students.map((student) => {
           const studentData = {
